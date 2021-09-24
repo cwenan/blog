ff770dc1b9cf04a275d5a51c4318d162ed4be6b0
cwenan <wenangongzi@gmail.com>
2021-09-24 21:51:51 +0800

    小修改

--- deploy.sh
+++ deploy.sh
@@ -27,18 +27,18 @@ git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 
-# deploy to coding pages
-# echo 'www.wenancoding.com\wenancoding.com' > CNAME  # 自定义域名
-## echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-#
-# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@e.coding.net:wenancoding/blog/blog.git
-# else
-#   codingUrl=https://LmOYoeimXx:${CODING_TOKEN}@e.coding.net/wenancoding/blog/blog.git
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
-#
+ deploy to coding pages
+ echo 'www.wenancoding.com\wenancoding.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+ if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+   codingUrl=git@e.coding.net:wenancoding/blog/blog.git
+ else
+   codingUrl=https://LmOYoeimXx:${CODING_TOKEN}@e.coding.net/wenancoding/blog/blog.git
+ fi
+ git add -A
+ git commit -m "${msg}"
+ git push -f $codingUrl master # 推送到coding
+
 cd -
 rm -rf docs/.vuepress/dist
